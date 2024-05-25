29eeeb9cabf3dae2cd9a4555242280d11cc4679d
yoga <yogaadi0902@gmail.com>
2024-05-25 20:55:17 +0700

    add database

--- app.mjs
+++ app.mjs
@@ -1,12 +1,29 @@
 import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "dtr",
+  password: "yoga65",
+  port: 5432,
+});
+
 const app = express();
 const port = 3000;
+db.connect();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", async (req, res) => {
+  const data = await db.query("SELECT * FROM additional_course");
+  const datarows = data.rows;
+  res.render("index", {
+    addCourse: datarows,
+  });
 });
 
 app.listen(port, () => {
